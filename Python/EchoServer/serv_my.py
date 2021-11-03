b0b6df4cae3a3edcd73ca5e46b7318c13cb11f1d
noble6 <bitcoineazy@gmail.com>
2021-11-03 22:16:49 +0300

    Echo server update

--- Python/EchoServer/serv_my.py
+++ Python/EchoServer/serv_my.py
@@ -3,13 +3,17 @@ import random
 import socket
 from threading import Thread
 import logging
-from utilities import COLORS, EMOJIS, EMOJIS_PATTERN, SocketMethods, Security
+import hashlib
 
 logging.basicConfig(filename='server_logs.log', level=logging.INFO)
 
 users = {}
 logins = {}
 connections_list = []
+ENCODING = 'utf-8'
+SALT = 'salt'.encode('utf-8')
+COLORS = ['\33[31m', '\33[32m', '\33[33m', '\33[34m', '\33[35m', '\33[36m', '\33[91m', '\33[92m', '\33[93m', '\33[94m',
+          '\33[95m', '\33[96m']
 
 
 class ClientThread(Thread):
@@ -28,13 +32,13 @@ class ClientThread(Thread):
         self.username = name
         if name in users.keys():
             self.send_msg('Enter password')
-            if users[name]['password'] == Security.get_password_hash(self.receive_msg()):
+            if users[name]['password'] == get_password_hash(self.receive_msg()):
                 self.success_login()
             else:
                 self.close_connection('incorrect password')
         else:
             self.send_msg('Set new password')
-            users.update({name: {'password': Security.get_password_hash(self.receive_msg())}})
+            users.update({name: {'password': get_password_hash(self.receive_msg())}})
             save_users()
         logins.update({addr[0]: name})
         save_logins()
@@ -43,12 +47,7 @@ class ClientThread(Thread):
         self.send_msg(f'Success login')
         save_users()
 
-    def close_connection(self, reason='', only_server=False):
-        if not only_server:
-            self.send_msg(f'Closing connection {self.addr} {" because of " + reason if reason else ""}')
-            self.send_msg('//close')
-            self.conn.close()
-        print(f'Connection closed {self.addr} {" - " + reason if reason else ""}')
+    def close_connection(self, reason=''):
         logging.info(f'Connection closed {self.addr} {" - " + reason if reason else ""}')
         self.connected = False
         if self in connections_list:
@@ -56,15 +55,15 @@ class ClientThread(Thread):
 
     def send_msg(self, message):
         if self.connected:
-            SocketMethods.send_text(self.conn, message)
+            send_text(self.conn, message)
 
     def receive_msg(self):
         if not self.connected:
             return
         try:
-            return SocketMethods.receive_text(self.conn)
+            return receive_text(self.conn)
         except ConnectionResetError:
-            self.close_connection('connection error', only_server=True)
+            self.close_connection('connection error')
 
     def run(self):
         connections_list.append(self)
@@ -76,17 +75,17 @@ class ClientThread(Thread):
             if message == 'exit':
                 self.close_connection('user exit')
                 break
-            send_msg_all(f'{self.color}{self.username}\33[0m: {emoji_replace(message)}')
+            send_msg_all(f'{self.color}{self.username}\33[0m: {message}')
 
 
 def save_users():
-    with open('users.json', 'w') as file:
-        json.dump(users, file)
+    with open('users.json', 'w') as f:
+        json.dump(users, f, indent=4)
 
 
 def save_logins():
-    with open('logins.json', 'w') as file:
-        json.dump(logins, file)
+    with open('logins.json', 'w') as f:
+        json.dump(logins, f, indent=4)
 
 
 def send_msg_all(message):
@@ -97,13 +96,17 @@ def service_msg(user, message):
     [i.send_msg(f'\33[4m{user.username} {message}\33[0m') for i in connections_list if i != user]
 
 
-def _emoji_replace(match):
-    mg = match.group(1)
-    return EMOJIS.get(mg, f':{mg}:')
+def get_password_hash(password):
+    return hashlib.sha512(password.encode('utf-8') + SALT).hexdigest()
 
 
-def emoji_replace(s):
-    return EMOJIS_PATTERN.sub(_emoji_replace, s)
+def receive_text(conn):
+    return conn.recv(1024).decode(ENCODING)
+
+
+def send_text(conn, message):
+    message = message.encode(ENCODING)
+    conn.send(message)
 
 
 if __name__ == '__main__':
@@ -118,9 +121,11 @@ if __name__ == '__main__':
     print(f'Started on {socket.gethostbyname(socket.gethostname())}:{port}')
     logging.info(f'Started on {socket.gethostbyname(socket.gethostname())}:{port}')
     sock.listen(10)
-
-    with open('users.json', 'r') as file:
-        users = json.load(file)
+    try:
+        with open('users.json', 'r') as file:
+            users = json.load(file)
+    except json.decoder.JSONDecodeError:
+        users = {}
     with open('logins.json', 'r') as file:
         logins = json.load(file)
     while True:
